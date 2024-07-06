67e08b7fc5317a347c9792362cf6f4de81c07d89
kuldeep <kuldeepdhakad152@gmail.com>
2024-07-06 10:16:20 +0530

    some shanges

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
     <nav className=" bg-green-900 text-white w-[100vw] h-20 absolute left-0 top-0 right-0 flex justify-between items-start p-4">
       <div className="flex items-start space-x-4">
         <img src="https://dynamic.brandcrowd.com/asset/logo/74e1a370-c7b8-42fb-b427-cc61ff946bc6/logo-search-grid-2x?logoTemplateVersion=1&v=638179880049600000&text=Education" alt="Logo" className="h-8 w-8"/>
-        <span className="font-bold text-lg">KASS</span>
+        <span className="font-bold text-lg">EduVentures</span>
       </div>
       <div className="flex space-x-8 items-start">
         <NavLink
